521d91c18d5f75d0dc60f7ce834c41cbad2e5159
Mayank Kaushik <kaushmayank218@gmail.com>
2025-11-03 22:05:16 +0530

    Videomeet.jsx Work

--- client/src/pages/VideoMeet.jsx
+++ client/src/pages/VideoMeet.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useRef,useState} from "react";
 import "../Styles/VideoMeet.css"
+import { TextField,Button } from '@mui/material';
 
 const server_url= "http://localhost:8000";
 //you can use useRef here as well so do how you will
@@ -49,9 +50,92 @@ const VideoMeet = () => {
     //todo
     //if(isChrome()===false){
     //}
+
+  const getPermissions = async() =>{
+    try{
+      const videoPermission = await navigator.mediaDevices.getUserMedia({video:true});
+      if(videoPermission){
+        setVideoAvailable(true);
+      }else{
+        setVideoAvailable(false);
+      }
+
+      const audioPermission = await navigator.mediaDevices.getUserMedia({audio:true});
+      if(audioPermission){
+        setAudioAvailable(true);
+      }else{
+        setAudioAvailable(false);
+      }
+
+      if(navigator.mediaDevices.getDisplayMedia){
+        setScreenAvailable(true);
+
+      }else{
+        setScreenAvailable(false);
+      }
+
+      if(videoAvailable || audioAvailable){
+        const userMediaStream = await navigator.mediaDevices.getUserMedia({video:videoAvailable,audio:audioAvailable});
+        if(userMediaStream){
+          window.localStream = userMediaStream;
+          if(localVideoRef.current){
+            localVideoRef.current.srcObject = userMediaStream;
+          }
+        }
+      }
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+    useEffect(()=>{
+       getPermissions();
+      //  console.log(localVideoRef);
+    },[]);
+    //this fn do like if user mute video then it will of it from every other user computer 
+    const getUserMediaSuccess =(stream)=>{
+
+    }
+
+   //here is that function below
+    let getUsermedia = ()=>{
+    if((video && videoAvailable) || (audio && audioAvailable)){
+      navigator.mediaDevices.getUserMedia({video:video,audio:audio})
+      .then(getUserMediaSuccess)//TODO:getUserMediaSuccess
+      .then((stream)=>{})
+      .catch((e)=>console.log(e))
+    }else{
+      try{
+        let tracks = localVideoRef.current.srcObject.getTracks();
+        tracks.forEach(track=>track.stop())
+      }catch(e){}
+    }};
+
+    //if some one changes like mute audio or off video then useEffect help to do so
+    useEffect(()=>{
+      if(video!==undefined && audio !== undefined){
+        getUserMedia();
+      }
+    },[audio,video])
+
+    let getMedia = () =>{
+      setVideo(videoAvailable);
+      setAudio(audioAvailable);
+
+      // connectToSocketServer();
+    }
+
   return (
     <div >
       { askForUsername ===true ? <div>
+
+        <h3>Enter Into Lobby</h3>
+        <TextField id="outlined-basic" label="Username" value={username} onChange={(e)=>setUsername(e.target.value)} variant="outlined" />
+          <Button variant="contained" onClick={connect}>Connect</Button>
+
+          <div>
+            <video  ref={localVideoRef} autoPlay muted></video>
+          </div>
         </div> :<></>
         }
     </div>
