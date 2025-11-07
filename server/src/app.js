7e3c7fb10eddc8501fdb73c54ff2a6507800897f
Mayank Kaushik <kaushmayank218@gmail.com>
2025-11-08 05:22:00 +0530

    Improvement Done

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,12 @@
-import express from "express" ;
+import express from "express";
 import dotenv from "dotenv";
-import {createServer} from "node:http";
-import {Server} from "socket.io";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import { connectToSocket } from "./controllers/socketManager.js";
-import userRoutes from "./routes/users.routes.js"
-//Recent Changes 
+import userRoutes from "./routes/users.routes.js";
+//Recent Changes
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -15,30 +15,31 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, ".env") });
 
-
 const app = express();
 const server = createServer(app);
-const io = connectToSocket(server);  // instead of this line as many things are in same app.js we create socketManager.js
+const io = connectToSocket(server); // instead of this line as many things are in same app.js we create socketManager.js
 
-const PORT = process.env.PORT || 8000
-
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit:"40kb",extended:true}))
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("/api/v1/users",userRoutes);
+app.use("/api/v1/users", userRoutes);
 // app.use("/api/v2/users",newUserRoutes);  like if someone using latest version or it updated
 
-const start = async() =>{
-   const connectionDB = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`Mongo Connected DB Host: ${connectionDB.connection.host}`);
-
-   server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+const start = async () => {
+  const connectionDB = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
   });
 
-}
+  console.log(`Mongo Connected DB Host: ${connectionDB.connection.host}`);
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+};
 
 start();
